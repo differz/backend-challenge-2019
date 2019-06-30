e893748537a42e8bc6772315aa30bce835b7a8d0
Sergii Suprun <s.o.suprun@gmail.com>
2019-07-01 00:06:27 +0300

    clean security config

--- src/main/java/com/differz/bc/config/SecurityConfiguration.java
+++ src/main/java/com/differz/bc/config/SecurityConfiguration.java
@@ -18,65 +18,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     private final UserDetailsService userDetailsService;
     private final PasswordEncoder passwordEncoder;
 
-    //   private static String REALM = "MY_TEST_REALM";
-
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.userDetailsService(userDetailsService)
-//                .passwordEncoder(passwordEncoder);
-//    }
-
-//    @Override
-    //   protected void configure(HttpSecurity http) throws Exception {
-//        http.authorizeRequests()
-//                .antMatchers("/**").permitAll() ///+? TODO secure
-//                .antMatchers("/login").permitAll()
-//                .anyRequest()
-//                .authenticated().and().csrf().disable().formLogin()
-//                .loginPage("/login").failureUrl("/login?error=true")
-//                .usernameParameter("username")
-//                .passwordParameter("password")
-//                .and().logout()
-//                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-//                .logoutSuccessUrl("/").and().exceptionHandling()
-//                .accessDeniedPage("/access-denied");
-
-//        http.authorizeRequests()
-//                .antMatchers("/login").permitAll()
-//                .anyRequest().authenticated()
-//                .and()
-//                .httpBasic()
-//                .authenticationEntryPoint(authenticationEntryPoint);
-//
-//        http.addFilterAfter(new CustomFilter(),
-//                BasicAuthenticationFilter.class);
-
-//        http.csrf().disable()
-//                .authorizeRequests()
-//            //    .antMatchers("/user/**").hasRole("ADMIN")
-//              //  .antMatchers("/login").permitAll()
-//                .anyRequest().authenticated()
-//                .and().httpBasic().realmName(REALM).authenticationEntryPoint(getBasicAuthEntryPoint())
-//                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-//    }
-
     @Override
     public void configure(WebSecurity web) {
         web.ignoring()
                 .antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**");
     }
-
-//    @Bean
-//    public CustomBasicAuthenticationEntryPoint getBasicAuthEntryPoint(){
-//        return new CustomBasicAuthenticationEntryPoint();
-//    }
-
-//    /* To allow Pre-flight [OPTIONS] request from browser */
-//    @Override
-//    public void configure(WebSecurity web) throws Exception {
-//        web.ignoring().antMatchers(HttpMethod.OPTIONS, "/**");
-//    }
-
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -88,9 +34,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.formLogin().disable();
-        http.httpBasic();
         http.authorizeRequests()
-                .antMatchers("/login").permitAll()
-                .anyRequest().authenticated();
+                .antMatchers("/users/**").authenticated()
+                .antMatchers("/rooms/**").authenticated();
     }
 }

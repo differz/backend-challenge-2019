0c429c1895a4d8b6d22319232b104fafbe57614a
Sergii Suprun <s.o.suprun@gmail.com>
2019-07-01 00:09:23 +0300

    remove postgres dialect to test with ram database

--- src/main/resources/db/migration/V20190623152900__create_messages.sql
+++ src/main/resources/db/migration/V20190623152900__create_messages.sql
@@ -8,12 +8,12 @@ CREATE TABLE IF NOT EXISTS messages
     CONSTRAINT messages_pkey
         PRIMARY KEY (id),
 
-    CONSTRAINT foreign_key_room
+    CONSTRAINT foreign_key_messages_room
         FOREIGN KEY (room_id)
             REFERENCES rooms (id)
             ON DELETE NO ACTION
             ON UPDATE NO ACTION,
-    CONSTRAINT foreign_key_user
+    CONSTRAINT foreign_key_messages_user
         FOREIGN KEY (user_id)
             REFERENCES users (id)
             ON DELETE NO ACTION

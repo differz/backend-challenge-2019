5209339f45ed483be2ef66beb1f1e13a5098b7a5
Sergii Suprun <s.o.suprun@gmail.com>
2019-07-08 23:10:49 +0300

    add opennlp bot

---
+++ src/main/java/com/differz/bc/chatbot/OpenNlpBotAnswerer.java
@@ -0,0 +1,81 @@
+package com.differz.bc.chatbot;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import opennlp.tools.doccat.DoccatModel;
+import opennlp.tools.doccat.DocumentCategorizerME;
+import opennlp.tools.lemmatizer.LemmatizerME;
+import opennlp.tools.lemmatizer.LemmatizerModel;
+import opennlp.tools.postag.POSModel;
+import opennlp.tools.postag.POSTaggerME;
+import opennlp.tools.tokenize.TokenizerME;
+import opennlp.tools.tokenize.TokenizerModel;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+
+import static com.differz.bc.chatbot.ChatbotService.BOT_KEY_PHRASE;
+
+@Slf4j
+@RequiredArgsConstructor
+public class OpenNlpBotAnswerer implements ChatbotAnswerer {
+
+    private final Map<String, String> answers;
+    private final DoccatModel model;
+
+    @Override
+    public String getAnswer(String message) {
+        String answer = null;
+        String msg = message.replace(BOT_KEY_PHRASE, "");
+        try {
+            String[] tokens = tokenize(msg);
+            String[] posTags = detectPOSTags(tokens);
+            String[] lemmas = lemmatize(tokens, posTags);
+            String category = detectCategory(lemmas);
+            answer = answers.get(category);
+        } catch (IOException e) {
+            log.error("can't get answer", e);
+        }
+        if (answer == null) {
+            answer = "i don't understand";
+        }
+        return answer;
+    }
+
+    private String[] tokenize(String sentence) throws IOException {
+        try (InputStream modelIn = new FileInputStream("en-token.bin")) {
+            TokenizerME tokenizerME = new TokenizerME(new TokenizerModel(modelIn));
+            String[] tokens = tokenizerME.tokenize(sentence);
+            log.info("Tokenizer : " + String.join(" | ", tokens));
+            return tokens;
+        }
+    }
+
+    private String[] detectPOSTags(String[] tokens) throws IOException {
+        try (InputStream modelIn = new FileInputStream("en-pos-maxent.bin")) {
+            POSTaggerME posTaggerME = new POSTaggerME(new POSModel(modelIn));
+            String[] posTokens = posTaggerME.tag(tokens);
+            log.info("POS Tags : " + String.join(" | ", posTokens));
+            return posTokens;
+        }
+    }
+
+    private String[] lemmatize(String[] tokens, String[] posTags) throws IOException {
+        try (InputStream modelIn = new FileInputStream("en-lemmatizer.bin")) {
+            LemmatizerME lemmatizerME = new LemmatizerME(new LemmatizerModel(modelIn));
+            String[] lemmaTokens = lemmatizerME.lemmatize(tokens, posTags);
+            log.info("Lemmatizer : " + String.join(" | ", lemmaTokens));
+            return lemmaTokens;
+        }
+    }
+
+    private String detectCategory(String[] finalTokens) {
+        DocumentCategorizerME categorizerME = new DocumentCategorizerME(model);
+        double[] probabilitiesOfOutcomes = categorizerME.categorize(finalTokens);
+        String category = categorizerME.getBestCategory(probabilitiesOfOutcomes);
+        log.info("Category: " + category);
+        return category;
+    }
+}
--- src/main/java/com/differz/bc/config/ChatbotConfiguration.java
+++ src/main/java/com/differz/bc/config/ChatbotConfiguration.java
@@ -2,13 +2,22 @@ package com.differz.bc.config;
 
 import com.differz.bc.chatbot.Chatbot;
 import com.differz.bc.chatbot.ChatbotAnswerer;
-import com.differz.bc.chatbot.EchoBotAnswerer;
+import com.differz.bc.chatbot.OpenNlpBotAnswerer;
 import com.differz.bc.core.User;
 import com.differz.bc.web.user.UserService;
 import lombok.RequiredArgsConstructor;
+import opennlp.tools.doccat.*;
+import opennlp.tools.util.*;
+import opennlp.tools.util.model.ModelUtil;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @EnableScheduling
@@ -28,6 +37,29 @@ public class ChatbotConfiguration {
 
     @Bean
     public ChatbotAnswerer chatbotAnswerer() {
-        return new EchoBotAnswerer();
+        return new OpenNlpBotAnswerer(categoryAnswers(), categoryModel());   // or new EchoBotAnswerer();
+    }
+
+    private DoccatModel categoryModel() {
+        try {
+            InputStreamFactory inputStreamFactory = new MarkableFileInputStreamFactory(new File("category.txt"));
+            ObjectStream<String> lineStream = new PlainTextByLineStream(inputStreamFactory, StandardCharsets.UTF_8);
+            ObjectStream<DocumentSample> sampleStream = new DocumentSampleStream(lineStream);
+            DoccatFactory factory = new DoccatFactory(new FeatureGenerator[]{new BagOfWordsFeatureGenerator()});
+            TrainingParameters params = ModelUtil.createDefaultTrainingParameters();
+            params.put(TrainingParameters.CUTOFF_PARAM, 0);
+            return DocumentCategorizerME.train("en", sampleStream, params, factory);
+        } catch (IOException e) {
+            throw new RuntimeException("can't get category model");
+        }
+    }
+
+    private Map<String, String> categoryAnswers() {
+        Map<String, String> answers = new HashMap<>();
+        answers.put("default", "Sorry, I didn’t understand - could you please rephrase?");
+        answers.put("greeting", "Hello! How can i help you?");
+        answers.put("continue", "Want something else?");
+        answers.put("complete", "Nice to meet you.");
+        return answers;
     }
 }
